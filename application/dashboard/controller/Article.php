6ec70c6e09e9fd963e3477248d66621c558e24f9
Zhang ChenYun <itzcy@itzcy.com>
2018-02-09 18:14:14 +0800

    feat: 增加前台模块

---
+++ application/dashboard/controller/Article.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Itzcy
+ * Date: 2018/2/19
+ * Time: 10:26
+ */
+
+namespace app\dashboard\controller;
+
+use think\Db;
+use think\Request;
+use think\Response;
+
+class Article extends Base
+{
+    /**
+     * 列表页
+     * @return mixed
+     */
+    public function index ()
+    {
+        $searchConfig = [
+            'query' => ['search' => input('get.search', '', 'trim')]
+        ];
+        $where = [];
+        $article = Db::name('article') -> where($where) -> order('art_id', 'asc') -> paginate('','', $searchConfig);
+        $this -> assign('article', $article);
+        $this -> assign('totalRows', Db::name('article') -> count());
+        $this -> assign('page', $article -> render());
+        $this -> assign('pageTotal', $article -> lastPage());
+        return $this -> fetch();
+    }
+
+    public function art_enable_or_disable()
+    {
+        if (Request::instance() -> isAjax()) {
+            $article = Db::name('article') -> getByArtId(input('get.art_id',0,'intval'));
+            dump($article);
+        } else {
+            return error('Method Error');
+        }
+    }
+
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\index\controller;
+
+class Index{
+    public function index()
+    {
+        echo 'App module.';
+	    echo '闲杂人等请速速离开！！！';
+    }
+}
