658ab0cb88caabd51689cb99867e53511ac58a47
itzcy <itzcy@qq.com>
2019-11-21 18:29:43 +0800

    301 重定向 兼容旧版

--- route/redirect.php
+++ route/redirect.php
@@ -2,5 +2,5 @@
 return [
     'blog/:id' => 'redirect/blog/location',
     'blog/category/blog/:sign' => 'redirect/category/location',
-    'blog/tag/:sign' => 'redirect/tag/location'
+    'tag/:sign' => 'redirect/tag/location',
 ];
