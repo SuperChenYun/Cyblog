991b7b5691ebb095b196293da4a19ed6a637fd61
itzcy <itzcy@qq.com>
2019-11-21 17:37:33 +0800

    变动路由

--- route/index.php
+++ route/index.php
@@ -8,7 +8,7 @@ return [
     'article/:id' => 'index/article/show', // 文章详情
     'article' => 'index/article/index', // 文章列表
 
-    'category/:id' => 'index/category/articles', // 分类下的文章列表
+    'category/:sign' => 'index/category/articles', // 分类下的文章列表
     'category' => 'index/category/index', // 分类
 
     'about' => 'index/info/about', // 关于
